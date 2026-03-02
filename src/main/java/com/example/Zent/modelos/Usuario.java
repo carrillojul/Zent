ec677020ecd3eca7157e24047d6ae7b4b4284e98
camila ortiz <camilalampion97@gmail.com>
2026-03-01 22:51:08 -0500

    Usuarios y Gastos terminados con relacion entre tablas

--- src/main/java/com/example/Zent/modelos/Usuario.java
+++ src/main/java/com/example/Zent/modelos/Usuario.java
@@ -1,8 +1,144 @@
 package com.example.Zent.modelos;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "usuarios")
+
 public class Usuario {
-    // adicionar 5 campos considerados importantes
-    // id, nombres, tipo de documento, documento, edad
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String nombres;
+    private String tipoDocumento;
+    private String documento;
+    private Integer edad;
+    private String correo;
+    private LocalDate fechaNacimiento;
+    private String telefono;
+    private LocalDate fechaRegistro;
+    private Boolean estado;
+
+    @OneToMany(mappedBy = "usuario", fetch = FetchType.LAZY)
+    private List<Gasto> gastos;
+
+    public Usuario() {
+    }
+
+    public Usuario(Integer id, String correo, String documento, Integer edad, Boolean estado, LocalDate fechaNacimiento, LocalDate fechaRegistro, String nombres, String telefono, String tipoDocumento) {
+        this.id = id;
+        this.correo = correo;
+        this.documento = documento;
+        this.edad = edad;
+        this.estado = estado;
+        this.fechaNacimiento = fechaNacimiento;
+        this.fechaRegistro = fechaRegistro;
+        this.nombres = nombres;
+        this.telefono = telefono;
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public Integer getid() {
+        return id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public LocalDate getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(LocalDate fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public Boolean getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Boolean estado) {
+        this.estado = estado;
+    }
+
+    public List<Gasto> getGastos() {
+        return gastos;
+    }
+
+    public void setGastos(List<Gasto> gastos) {
+        this.gastos = gastos;
+    }
 
     
 }
